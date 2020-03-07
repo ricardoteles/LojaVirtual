039a316104310252ea9cf7cbe39dbc2888150d4f
ricardoteles <ricardo.oliveira.teles@gmail.com>
2020-03-07 17:01:12 -0300

    Criando as páginas e a lógica de Contatos

---
+++ Libraries/Email/ContatoEmail.cs
@@ -0,0 +1,42 @@
+﻿using LojaVirtual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Libraries.Email
+{
+    public class ContatoEmail
+    {
+        public static void EnviarContatoEmail(Contato contato) {
+            /*
+             * SMTP -> Servidor que vai enviar a mensagem.
+             */
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("ricardo.oliveira.teles@gmail.com", "");
+            smtp.EnableSsl = true;
+
+            string corpoMsg = string.Format("<h2>Contato - LojaVirtual</h2>" +
+                "<b>Nome:</b> {0} <br/>" +
+                "<b>E-mail:</b> {1} <br/>" +
+                "<b>Texto:</b> {2} <br/>" +
+                "<br/> E-mail enviado automaticamente do site LojaVirtual.",
+                contato.Nome, contato.Email, contato.Texto);
+            /*
+             * MailMessage -> Construir a mensagem.
+             */
+            MailMessage mensagem = new MailMessage();
+            mensagem.From = new MailAddress("ricardo.oliveira.teles@gmail.com");
+            mensagem.To.Add("ricardo.oliveira.teles@gmail.com");
+            mensagem.Subject = "Contato - LojaVirtual - E-mail " + contato.Email;
+            mensagem.Body = corpoMsg;
+            mensagem.IsBodyHtml = true;
+
+            // Enviar mensagem via SMTP
+            smtp.Send(mensagem);
+        }
+    }
+}
